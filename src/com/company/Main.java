2b6743825c84078ec8e41904ba4833cc29893154
Egor BIzyaev <egorbizyaev123@gmail.com>
2020-01-04 03:56:14 +0400

    add solution task

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import main.Decode;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Decode decode = new Decode();
+        decode.getFilesInConsole();
+    }
+}
